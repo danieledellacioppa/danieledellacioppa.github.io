3ef52a81bbd7bc2a0d3fe333cd599159626729bb
Daniele Della Cioppa <daniele.dellacioppa@gmail.com>
2024-03-14 20:11:40 +0000

    prova

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
-// Non c'è bisogno di differenziare tra sviluppo e produzione in questo caso
-const base = '/';
+import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
-  base, // Uso la variabile 'base' qui, ma potresti anche semplicemente scrivere 'base: "/"'
+  base: process.env.NODE_ENV === 'production' ? '/danieledellacioppa.github.io/' : '/',
 });
