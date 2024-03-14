6ca1b950971901b313a48cb2e7fe836a255fd225
Daniele Della Cioppa <daniele.dellacioppa@gmail.com>
2024-03-14 21:38:41 +0000

    jojo

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
 
 export default defineConfig({
-  plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/danieledellacioppa.github.io/' : '/',
+  plugins: [vue()]
 });
